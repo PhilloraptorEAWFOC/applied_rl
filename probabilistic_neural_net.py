35ca1ae19b8c3cd2ab0e7d4b9fc16d2c97c38bf9
MyCodeIsland <39490763+MyCodeIsland@users.noreply.github.com>
2021-01-19 17:40:22 +0100

    PNN basic outline -> combine with data

--- probabilistic_neural_net.py
+++ probabilistic_neural_net.py
@@ -1,6 +1,7 @@
 import os
-import torch as T
+import torch as torch
 import torch.nn as nn
+from tensorflow_probability import math
 
 from torch.distributions.normal import Normal
 from torch import nn
@@ -9,50 +10,170 @@ import torch.nn.functional as F
 import numpy as np
 from torch.nn import Module
 from torch.nn.modules import activation
+import gym
+
+#https://www.nitarshan.com/bayes-by-backprop/
+'''we apply Bayes rule onto the weights w of a neural net, given data D
+how does a neural network turn probabilistic?
+
+each of its weights is represented by a distribution rather than a single numeric value
+neural network can then be seen as a probabilistic model P(y| x, w)
+
+we view data as fixed and model weights as random variable 
+
+-> train to maximize the posterior
+ -> when we have the weights, prediction can be viewed as using model with learned fixed weights
+inference = process of computing the entire posterior distribution P(w|D) -> we can perform prediction by taking a weighted
+expectation over all possible values for w'''
 
 
-class Net(nn.Module):
-    def __init__(self, lr, input_dims, fc2_dims, n_actions):
-        super(Net, self).__init__()
-        self.input_dims = input_dims,
-        self.fc2_dims = fc2_dims,
-        self.n_actions = n_actions,
-        self.optimizer = optim.Adam(self.parameters(), lr=lr)
-
-        # negtive log likelihood
-        self.loss = nn.NLLLoss
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cuda:1')
-        self.to(self.device)
-
-    def forward(self, observation):
-        state = T.Tensor(observation).to(self.device)
-        x = F.relu(self.fc1(state))
-        y = F.relu(self.fc2(x))
-        actions = self.fc3(y)
-
-        return actions
-
-
-#a keras dense layer has two components: kernel matrix and bias vector
-#lern distributon over weights
-
-#bayesian weight uncertainty: tfp.layers.densevariations - unknown unknowns: epistemic uncertainty; not sure what data is not telling me
-
-#https://www.youtube.com/watch?v=X6pCO0-HYVE
+'''https://medium.com/@Cambridge_Spark/neural-networks-in-python-from-sklearn-to-pytorch-and-probabilistic-neural-networks-70b1c77e82da
+[...] as the word Bayesian indicates, we impose a prior on the network parameters. 
+We usually define the distribution (e.g. Normal) and we initialise the parameters with the prior. 
+In this case, we use an engineered mixture of Gaussians for the prior.
+we need VARIATIONAL INFERENCE
 '''
-def net():
- model = keras.Sequential([
-        tfp.layers.DenseVariational(64, activtion='relu', input_shape = [len(train_dataset.keys())]),
-        layers.Dense(64, activation='relu'),
-        layers.Dense(1)
-    ])
 
-    optimizer = tf.keras.optimizers.RMSprop(0.001)
+PI = 0.5
+SIGMA_1 = torch.cuda.FloatTensor([math.exp(-0)])
+SIGMA_2 = torch.cuda.FloatTensor([math.exp(-6)])
 
-    model.compile(loss='mse',
-                  optimizer=optimizer,
-                  metrics=['mae', 'mse'])
+class Gaussian(object):
+    def __init__(self, mu, rho):
+        super().__init__()
+        self.mu = mu
+        self.rho = rho
+        self.normal = torch.distributions.Normal(0, 1)
 
-    return model
-'''
+    @property
+    def sigma(self):
+        return torch.log1p(torch.exp(self.rho))
+
+    def sample(self):
+        epsilon = self.normal.sample(self.rho.size()).to(self.device)
+        return self.mu + self.sigma * epsilon
+
+    def log_prob(self, input):
+        return (-math.log(math.sqrt(2 * math.pi))
+                - torch.log(self.sigma)
+                - ((input - self.mu) ** 2) / (2 * self.sigma ** 2)).sum()
+
+
+
+class ScaleMixtureGaussian(object):
+    def __init__(self, pi, sigma1, sigma2):
+        super().__init__()
+        self.pi = pi
+        self.sigma1 = sigma1
+        self.sigma2 = sigma2
+        self.gaussian1 = torch.distributions.Normal(0, sigma1)
+        self.gaussian2 = torch.distributions.Normal(0, sigma2)
+
+    def log_prob(self, input):
+        prob1 = torch.exp(self.gaussian1.log_prob(input))
+        prob2 = torch.exp(self.gaussian2.log_prob(input))
+        return (torch.log(self.pi * prob1 + (1 - self.pi) * prob2)).sum()
+
+
+
+#single Bayesian network layer
+
+class BayesianLinear(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+
+        # Weight parameters
+        self.weight_mu = nn.Parameter(torch.Tensor(out_features, in_features).uniform_(-0.2, 0.2))
+        self.weight_rho = nn.Parameter(torch.Tensor(out_features, in_features).uniform_(-5,-4))
+        self.weight = Gaussian(self.weight_mu, self.weight_rho)
+
+        # Bias parameters
+        self.bias_mu = nn.Parameter(torch.Tensor(out_features).uniform_(-0.2, 0.2))
+        self.bias_rho = nn.Parameter(torch.Tensor(out_features).uniform_(-5,-4))
+        self.bias = Gaussian(self.bias_mu, self.bias_rho)
+
+        # Prior distributions
+        self.weight_prior = ScaleMixtureGaussian(PI, SIGMA_1, SIGMA_2)
+        self.bias_prior = ScaleMixtureGaussian(PI, SIGMA_1, SIGMA_2)
+        self.log_prior = 0
+        self.log_variational_posterior = 0
+
+
+    def forward(self, input, sample=False, calculate_log_probs=False):
+        if self.training or sample:
+            weight = self.weight.sample()
+            bias = self.bias.sample()
+        else:
+            weight = self.weight.mu
+            bias = self.bias.mu
+        if self.training or calculate_log_probs:
+            self.log_prior = self.weight_prior.log_prob(weight) + self.bias_prior.log_prob(bias)
+            self.log_variational_posterior = self.weight.log_prob(weight) + self.bias.log_prob(bias)
+        else:
+            self.log_prior, self.log_variational_posterior = 0, 0
+
+        return F.linear(input, weight, bias)
+
+
+
+
+
+
+#full 2-layer fully connected neural network model
+class BayesianNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.l1 = BayesianLinear(28 * 28, 400)
+        self.l2 = BayesianLinear(400, 400)
+        self.l3 = BayesianLinear(400, 10)
+
+    def forward(self, x, sample=False):
+        x = x.view(-1, 28 * 28)
+        x = F.relu(self.l1(x, sample))
+        x = F.relu(self.l2(x, sample))
+        x = F.log_softmax(self.l3(x, sample), dim=1)
+        return x
+
+    def log_prior(self):
+        return self.l1.log_prior \
+               + self.l2.log_prior \
+               + self.l3.log_prior
+
+    def log_variational_posterior(self):
+        return self.l1.log_variational_posterior \
+               + self.l2.log_variational_posterior \
+               + self.l3.log_variational_posterior
+
+    def sample_elbo(self, input, target, samples=SAMPLES):
+        outputs = torch.zeros(samples, BATCH_SIZE, CLASSES).to(DEVICE)
+        log_priors = torch.zeros(samples).to(DEVICE)
+        log_variational_posteriors = torch.zeros(samples).to(DEVICE)
+        for i in range(samples):
+            outputs[i] = self(input, sample=True)
+            log_priors[i] = self.log_prior()
+            log_variational_posteriors[i] = self.log_variational_posterior()
+        log_prior = log_priors.mean()
+        log_variational_posterior = log_variational_posteriors.mean()
+        negative_log_likelihood = F.nll_loss(outputs.mean(0), target, size_average=False)
+        loss = (log_variational_posterior - log_prior) / NUM_BATCHES + negative_log_likelihood
+        return loss, log_prior, log_variational_posterior, negative_log_likelihood
+
+
+net = BayesianNetwork().to(DEVICE)
+
+#training
+def train(net, optimizer, epoch):
+    net.train()
+    for batch_idx, (data, target) in enumerate(tqdm(train_loader)):
+        net.zero_grad()
+        loss = net.sample_elbo(data, target)
+        loss.backward()
+        optimizer.step()
+
+optimizer = optim.Adam(net.parameters())
+for epoch in range(TRAIN_EPOCHS):
+    train(net, optimizer, epoch)
+
 
